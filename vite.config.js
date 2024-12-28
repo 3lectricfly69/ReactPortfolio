dfd03e5190aeeb62b035dda8db355c2fe8b5427e
Marlie Ford <jive.hive@outlook.com>
2024-12-27 23:15:42 -0600

    altering server port in vite configs to resolve tail logs error

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 plugins: [react()],
 server: {
-    port: 3000,
+    port: process.env.PORT || 3000,
     open: true
 },
 test: {
